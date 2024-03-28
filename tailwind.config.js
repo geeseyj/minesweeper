9c2153b5d58c1a666238f032c63a965d471fdf78
John Geesey <geeseyjohn@gmail.com>
2024-03-28 14:58:20 -0500

    add game colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // gColor stands for "game" color
+      colors: {
+      'gBlue': '#0000ff',
+      'gRed': '#ff0000',
+      'gGreen': '#008000',
+      'gMaroon': '#800000',
+      'gNavy': '#000080',
+      'gLightGray': '#c6c6c6',
+      'gDarkGray': '#808080',
+      'white': '#ffffff',
+      'gTeal': '#008080',
+      'black': '#000000',
+    }},
   },
   plugins: [],
-}
+};
 
